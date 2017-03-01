74f4ee82d96653e96685c5769bdb6f584ce1e109
nikkov <nikkov@gmail.com>
2017-03-01 13:01:51 +0600

    Cubietruck pin mapping

--- source/common.c
+++ source/common.c
@@ -31,78 +31,101 @@ int revision = -1;
 
 extern int f_a20;
 
+//Cubietruck CN8
+/*
+	fex
+	
+[gpio_para]
+	gpio_used = 21
+	gpio_num = 4
+	;pin rezerved onboard hardware
+	gpio_pin_1 = port:PH20<1><default><default><1>
+	gpio_pin_2  = port:PH10<0><default><default><0>
+	
+	;CN8 pin
+	gpio_pin_2 = port:PC19<1><default><default><default>
+	gpio_pin_3 = port:PC21<1><default><default><default>
+	gpio_pin_4 = port:PC20<1><default><default><default>
+	gpio_pin_5 = port:PC22<1><default><default><default>
+	gpio_pin_6 = port:PB14<1><default><default><default>
+	gpio_pin_7 = port:PB16<1><default><default><default>
+	gpio_pin_8 = port:PB15<1><default><default><default>
+	gpio_pin_9 = port:PB17<1><default><default><default>
+	gpio_pin_10 = port:PI20<1><default><default><default>
+	gpio_pin_11 = port:PI14<1><default><default><default>
+	gpio_pin_12 = port:PI21<1><default><default><default>
+	gpio_pin_13 = port:PI15<1><default><default><default>
+	gpio_pin_14 = port:PI03<1><default><default><default>
+	gpio_pin_15 = port:PB03<1><default><default><default>
+	gpio_pin_16 = port:PB02<1><default><default><default>
+	gpio_pin_17 = port:PB18<1><default><default><default>
+	gpio_pin_18 = port:PB19<1><default><default><default>
+	
+	;pin rezerved BT hardware
+	gpio_pin_68 = port:PH18<0><default><default><0>
+	gpio_pin_69 = port:PH24<0><default><default><0>	
+	
+*/
 const int physToGpio_BP [64] =		//BOARD MODE
 {
 	-1, // 0
 	-1, -1, //1, 2
-	53, -1, //3, 4
-	52, -1, //5, 6
-	226, 228, //7, 8
-	-1, 229, //9, 10
-	275, 259, //11, 12
-	274, -1, //13, 14
-	273, 244, //15, 16
-	-1, 245, //17, 18
-	268, -1, //19, 20
-	269, 272, //21, 22
-	267, 266, //23, 24
-	-1, 270, //25, 26
-	257, 256, //27, 28
-	35, -1, //29, 30
-	277, 276, //31, 32
-	45, -1, //33, 34
-	39, 38, //35, 36
-	37, 44, //37, 38
-	-1, 40, //39, 40
+	-1, -1, //3, 4
+	83, 85, //5, 6
+	84, 86, //7, 8
+	46, 48, //9, 10
+	47, 49, //11, 12
+	-1, -1, //13, 14
+	276, 270, //15, 16
+	277, 271, //17, 18
+	259, 35, //19, 20
+	34, -1, //21, 22
+	50, -1, //23, 24
+	51, -1, //25, 26
+	-1, -1, //27, 28
+	-1, -1, //29, 30
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, //31-> 40
 	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, //41-> 55
 	-1, -1, -1, -1, -1, -1, -1, -1 // 56-> 63
 } ;
 const int pinTobcm_BP [64] =	//BCM MODE
 {
-	257,256, //map to BCM GPIO0,1
-	53,52, //map to BCM GPIO2,3
-	226,35, //map to BCM GPIO4,5
-	277,270, //map to BCM GPIO6,7
-	266,269, //map to BCM GPIO8,9
-	268,267, //map to BCM GPIO10,11
-	276,45, //map to BCM GPIO12,13
-	228,229, //map to BCM GPIO14,15
-	38,275, //map to BCM GPIO16,17
-	259,39, //map to BCM GPIO18,19
-	44, 40, //map to BCM GPIO20,21
-	273,244, //map to BCM GPIO22,23
-	245,272, //map to BCM GPIO24,25
-	37, 274, //map to BCM GPIO26,27
-	-1,-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 28... 43
+	-1,-1, //skip GPIO0,1
+	83,85, //map to BCM GPIO2,3
+	84,86, //map to BCM GPIO4,5
+	46,48, //map to BCM GPIO6,7
+	47,49, //map to BCM GPIO8,9
+	276,270, //map to BCM GPIO10,11
+	277,271, //map to BCM GPIO12,13
+	259,35, //map to BCM GPIO14,15
+	34,50, //map to BCM GPIO16,17
+	51,-1, //map to BCM GPIO18,19
+	-1, -1, -1, -1, -1,-1, -1,-1, // 20... 27
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 28... 43
 	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, //44... 59
 	-1, -1, -1, -1 // ...63
 } ;
 const int physToGpioR3 [64] =//head num map to BCMpin
 {
 	-1, // 0
-	-1, -1, // 1, 2
-	2, -1,
-	3, -1,
-	4, 14,
-	-1, 15,
-	17, 18,
-	27, -1,
-	22, 23,
-	-1, 24,
-	10, -1,
-	9, 25,
-	11, 8,
-	-1, 7, // 25, 26
-	0, 1, //27, 28
-	5, -1, //29, 30
-	6, 12, //31, 32
-	13, -1, //33, 34
-	19, 16, //35, 36
-	26, 20, //37, 38
-	-1, 21, //39, 40
-	// Padding:
-	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // ... 56
-	-1, -1, -1, -1, -1, -1, -1, // ... 63
+	-1, -1, //1, 2
+	-1, -1, //3, 4
+	2, 3, //5, 6
+	4, 5, //7, 8
+	6, 7, //9, 10
+	8, 9, //11, 12
+	-1, -1, //13, 14
+	10, 11, //15, 16
+	12, 13, //17, 18
+	14, 15, //19, 20
+	16, -1, //21, 22
+	17, -1, //23, 24
+	18, -1, //25, 26
+	-1, -1, //27, 28
+	-1, -1, //29, 30
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, //31-> 40
+	-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, //41-> 55
+	-1, -1, -1, -1, -1, -1, -1, -1 // 56-> 63
 } ;
 
 
@@ -145,9 +168,9 @@ int get_gpio_number(int channel, unsigned int *gpio,unsigned int *sys_gpio)
 {
 	// check channel number is in range
 	if ( (gpio_mode == BCM && (channel < 0 || channel > 64))
-	|| (gpio_mode == BOARD && (channel < 1 || channel > 64)) )
+	|| (gpio_mode == BOARD && (channel < 1 || channel > 30)) )
 	{
-		PyErr_SetString(PyExc_ValueError, "The channel sent is invalid on a Banana Pi");
+		PyErr_SetString(PyExc_ValueError, "The channel sent is invalid on a Cubietruck");
 		return 4;
 	}
 
@@ -156,7 +179,7 @@ int get_gpio_number(int channel, unsigned int *gpio,unsigned int *sys_gpio)
 	{
 		if (*(*pin_to_gpio+channel) == -1)
 		{
-			PyErr_SetString(PyExc_ValueError, "The channel sent is invalid on a Banana Pi");
+			PyErr_SetString(PyExc_ValueError, "The channel sent is invalid on a Cubietruck");
 			return 5;
 		} 
 		else 
